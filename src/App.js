22d1767ac8d6fafa80c27f5dc34005804684626e
HoangDCV <HoangDCV1998@gmail.com>
2022-03-08 15:25:57 +0700

    update todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [job, setJob]= useState('')
+  const handlerAddJob = () => {
+    setJobs( prev => [...prev, job])
+    setJob('')
+  }
+  const handleDelete = (jobDelete) => {
+    setJobs(jobs.filter(job=> job !== jobDelete))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-todo' style={{padding : 50}}>
+      <input 
+        value={job}
+        onChange={e => setJob(e.target.value)}
+      />
+      <button onClick={handlerAddJob}>Add</button>
+
+      <ul>
+      {
+        jobs.map((job, index) => (
+          <li key={index}>{job} 
+            <button onClick={() => handleDelete(job)}>Delete</button>
+          </li>
+        ))
+      }
+      </ul>
     </div>
   );
 }
